18f49f64fe2cc8eee91778cef767dd2c67ab8925
MatheusCataneo <matth-syd@hotmail.com>
2017-05-24 00:05:57 -0300

    agora vai

---
+++ app/src/main/java/Modules/DirectionFinderListener.java
@@ -0,0 +1,14 @@
+package Modules;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Modules.Route;
+
+
+public interface DirectionFinderListener {
+    void onDirectionFinderStart();
+    void onDirectionFinderSuccess(List<Route> route);
+}
---
+++ app/src/main/java/Modules/Route.java
@@ -0,0 +1,18 @@
+package Modules;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.List;
+
+
+public class Route {
+    public Distance distance;
+    public Duration duration;
+    public String endAddress;
+    public LatLng endLocation;
+    public String startAddress;
+    public LatLng startLocation;
+
+    public List<LatLng> points;
+    public List<LatLng> steps;
+}
